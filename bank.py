302ba41f28a6a992811ba0fc0270927e285a054f
Winfred Grace Kendi <79199127+KendiGrace@users.noreply.github.com>
2021-06-11 22:29:24 +0300

    Add files via upload

--- bank.py
+++ bank.py
@@ -9,20 +9,29 @@ class Account:
             
 
       def deposit(self,deposit):
-    
-          if deposit<0:
+        try:
+            1+deposit
+        except TypeError:
+            return "The amount should be in figures"
+        if deposit<0:
               return "The amount has to be greater than zero"
-          else:
-            self.balance+=deposit
-            transaction={"amount":deposit,"balance":self.balance,"time":datetime.now(),"narration":"deposit"}
-            self.transactions.append(transaction)
+        else:
+                self.balance+=deposit
+           
+        transaction={"amount":deposit,"balance":self.balance,"time":datetime.now(),"narration":"deposit"}
+        self.transactions.append(transaction)
          
-          return "Deposited amount:{} at {}.Your new your account balance  is  {}.".format(deposit,datetime.now(),self.balance)
+        return "Deposited amount:{} at {}.Your new your account balance  is  {}.".format(deposit,datetime.now(),self.balance)
       def save(self,savings):
            self.savings=0.0
            return "I saved up {} yesterday,my new savings account balance is {}.".format(savings,self.savings)
       
       def withdrawal(self,amount):
+          try:
+              1+amount
+          except TypeError:
+              return "The amount must be in figures"
+
           if amount<0:
               
               return "The amount must be greater than zero"
@@ -61,6 +70,7 @@ class Account:
                 return "you have successfully paid {}.Your new loan balance is {}.".format(repay,self.loan)
           elif repay>self.loan:
                repay-=self.loan
+               self.loan=0
                extra=repay-self.loan
                self.balance+=extra
                repayment={"amount":repay,"balance":self.balance,"time":datetime.now(),"narration":"repayment"}
@@ -76,5 +86,39 @@ class Account:
             time=transaction["time"]
             print("{} {}: {}.New balance:{}".format(time,narration,amount,balance))
 
-    
+      def transfer(self,transfer,Account):
+          try:
+              1+transfer
+          except TypeError:
+              return "The amount you are sending must be in figures"
+
+          fees=0.05*transfer
+          total_transfer=transfer+ fees 
+              
+          if total_transfer<0:
+           return "Insufficient funds"
+          elif total_transfer>self.balance:
+            return "Your balance is {} and you need {} to transfer money.".format(self.balance,total_transfer)
+          else:
+              self.balance-=total_transfer
+              return "You have transferred {} to {}.Your new balance is {}".format(transfer,Account,self.balance)
+
+class mobileMoney(Account):
+    def __init__(self,name,number,password,service_provider):
+        Account.__init__(self,name,number,password)
+        self.service_provider=service_provider
+        self.limit=300000
+
+    def buy_airtime(self,amount):
+        if amount<self.balance:
+            amount-=self.balance
+            return "You have successfully bought {}.Your new balance is {}".format(amount,self.balance)
+        else:
+            return "You have insufficient funds to complete this request"
+
+
+                 
+
+                  
+
 
